27f22a4a36cf001b68375d045df52fa872b72287
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-02-23 20:45:51 +0000

    chore: release v1.6.0

--- tests/restore.simple.test.ts
+++ tests/restore.simple.test.ts
@@ -23,7 +23,7 @@ describe('Restore Action', () => {
 
       await run();
 
-      expect(ensureBoringCache).toHaveBeenCalledWith({ version: 'v1.5.0' });
+      expect(ensureBoringCache).toHaveBeenCalled();
 
       expect(execBoringCache).toHaveBeenCalledWith(
         expect.arrayContaining(['restore', 'my-org/my-project', 'deps:node_modules,build:dist']),
